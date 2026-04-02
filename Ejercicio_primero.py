968055611e6b8a5bbcbe28af04f9ace24c928ea1
Dokari <ricardotejedor18@gmail.com>
2026-04-02 18:21:20 -0500

    Ejercicio

---
+++ Ejercicio_primero.py
@@ -0,0 +1,13 @@
+'''Alice, Bob y Carol han acordado juntar sus dulces de Halloween y repartirlos equitativamente entre ellos.
+Por el bien de su amistad, cualquier dulce que sobre será aplastado.
+Por ejemplo, si colectivamente llevan a casa 91 dulces, cada uno tomará 30 y aplastarán 1.
+
+Escribe una expresión aritmética a continuación para calcular cuántos dulces deben aplastar para una cantidad determinada.'''
+
+# Variables con el numero de dulces recogidos por alice, bob y carol
+alice_candies = 121
+bob_candies = 77
+carol_candies = 109
+
+# Tu codigo irá aquí abajo (la respuesta debe ser 1)
+# te en cuenta las variables ya asignadas
--- expresiones_aritmeticas.py
+++
@@ -1,5 +0,0 @@
-hat_height_cm = 25
-my_height_cm = 190
-# How tall am I, in meters, when wearing my hat?
-total_height_meters = hat_height_cm + my_height_cm
-print("Height in meters =", total_height_meters, "?")
