d7b88a0e8af88972a8839f1ea183e31dad274836
Dokari <ricardotejedor18@gmail.com>
2026-04-05 15:46:09 -0500

    Funciones, Booleanos y condicionales

---
+++ BooleanosCondiciones/Ejercicio.py
@@ -0,0 +1,12 @@
+# Aquí tienes una funcion simple de positivo y negativo, pero se puede optimizar a una linea
+# Intenta optimizarla a una sola linea...
+# Pista: solo necesitas cuatro 'palabras'
+
+def negativo(num):
+    if numr < 0:
+        return True
+    else:
+        return False
+
+def negative_optimizado(num):
+    pass #Borra esta palabra reservada y reemplazala por tu código
